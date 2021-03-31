78dac604177a62d36fdd4a9e08faa995df8dcd5b
Cole Gawin <colegawin@gmail.com>
2021-03-31 11:01:32 -0500

    completed lab06

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -8,7 +8,7 @@ class Stack:
     class Node:
         def __init__(self, val, next=None):
             self.val = val
-            self.next  = next
+            self.next = next
 
     def __init__(self):
         self.top = None
@@ -42,6 +42,7 @@ class Stack:
             yield n.val
             n = n.next
 
+
 ################################################################################
 # CHECK DELIMITERS
 ################################################################################
@@ -51,7 +52,22 @@ def check_delimiters(expr):
     delim_closers = '})]>'
 
     ### BEGIN SOLUTION
+    stack = Stack()
+
+    for d in expr:
+        if d in delim_openers:
+            stack.push(d)
+        elif d in delim_closers:
+            if stack.empty():
+                return False
+            if delim_openers.index(stack.peek()) == delim_closers.index(d):
+                stack.pop()
+            else:
+                return False
+
+    return stack.empty()
     ### END SOLUTION
+
 
 ################################################################################
 # CHECK DELIMITERS - TEST CASES
@@ -64,6 +80,7 @@ def test_check_delimiters_1():
     tc.assertTrue(check_delimiters('{}'))
     tc.assertTrue(check_delimiters('<>'))
 
+
 # points:5
 def test_check_delimiters_2():
     tc = TestCase()
@@ -72,6 +89,7 @@ def test_check_delimiters_2():
     tc.assertTrue(check_delimiters('{<()>}'))
     tc.assertTrue(check_delimiters('<({[]})>'))
 
+
 # points: 5
 def test_check_delimiters_3():
     tc = TestCase()
@@ -79,6 +97,7 @@ def test_check_delimiters_3():
     tc.assertTrue(check_delimiters('[{()} [] (<> <>) {}]'))
     tc.assertTrue(check_delimiters('{} <> () []'))
     tc.assertTrue(check_delimiters('<> ([] <()>) <[] [] <> <>>'))
+
 
 # points: 5
 def test_check_delimiters_4():
@@ -92,6 +111,7 @@ def test_check_delimiters_4():
     tc.assertFalse(check_delimiters('}'))
     tc.assertFalse(check_delimiters('>'))
 
+
 # points: 5
 def test_check_delimiters_5():
     tc = TestCase()
@@ -100,6 +120,7 @@ def test_check_delimiters_5():
     tc.assertFalse(check_delimiters('{ >'))
     tc.assertFalse(check_delimiters('< )'))
 
+
 # points: 5
 def test_check_delimiters_6():
     tc = TestCase()
@@ -107,6 +128,7 @@ def test_check_delimiters_6():
     tc.assertFalse(check_delimiters('((((((( ))))))'))
     tc.assertFalse(check_delimiters('< < > > >'))
     tc.assertFalse(check_delimiters('( [] < {} )'))
+
 
 ################################################################################
 # INFIX -> POSTFIX CONVERSION
@@ -121,8 +143,25 @@ def infix_to_postfix(expr):
     postfix = []
     toks = expr.split()
     ### BEGIN SOLUTION
+    for tok in toks:
+        if tok.isdigit():
+            postfix.append(tok)
+        elif tok == '(':
+            ops.push(tok)
+        elif tok == ')':
+            while not ops.empty() and ops.peek() != '(':
+                postfix.append(ops.pop())
+            ops.pop()
+        elif tok in prec:
+            while (not ops.empty()) and (ops.peek() != '(' and ops.peek() != ')') and prec[tok] <= prec[ops.peek()]:
+                postfix.append(ops.pop())
+            ops.push(tok)
+
+    while not ops.empty():
+        postfix.append(ops.pop())
     ### END SOLUTION
     return ' '.join(postfix)
+
 
 ################################################################################
 # INFIX -> POSTFIX CONVERSION - TEST CASES
@@ -137,6 +176,7 @@ def test_infix_to_postfix_1():
     tc.assertEqual(infix_to_postfix('1 + 2 - 3'), '1 2 + 3 -')
     tc.assertEqual(infix_to_postfix('1 + ( 2 - 3 )'), '1 2 3 - +')
 
+
 # points: 10
 def test_infix_to_postfix_2():
     tc = TestCase()
@@ -145,12 +185,14 @@ def test_infix_to_postfix_2():
     tc.assertEqual(infix_to_postfix('1 * 2 * 3 + 4'), '1 2 * 3 * 4 +')
     tc.assertEqual(infix_to_postfix('1 + 2 * 3 * 4'), '1 2 3 * 4 * +')
 
+
 # points: 10
 def test_infix_to_postfix_3():
     tc = TestCase()
     tc.assertEqual(infix_to_postfix('1 * ( 2 + 3 ) * 4'), '1 2 3 + * 4 *')
     tc.assertEqual(infix_to_postfix('1 * ( 2 + 3 * 4 ) + 5'), '1 2 3 4 * + * 5 +')
     tc.assertEqual(infix_to_postfix('1 * ( ( 2 + 3 ) * 4 ) * ( 5 - 6 )'), '1 2 3 + 4 * * 5 6 - *')
+
 
 ################################################################################
 # QUEUE IMPLEMENTATION
@@ -161,24 +203,56 @@ class Queue:
         self.head = -1
         self.tail = -1
 
-    ### BEGIN SOLUTION
-    ### END SOLUTION
-
     def enqueue(self, val):
         ### BEGIN SOLUTION
+        if self.empty():
+            self.tail = 0
+        self.head += 1
+
+        if self.head >= len(self.data):
+            self.head = 0
+
+        if self.data[self.head] is not None:
+            raise RuntimeError
+
+        self.data[self.head] = val
         ### END SOLUTION
 
     def dequeue(self):
         ### BEGIN SOLUTION
+        if self.empty():
+            raise RuntimeError
+
+        val = self.data[self.tail]
+        self.data[self.tail] = None
+        self.tail += 1
+
+        if self.tail >= len(self.data):
+            self.tail = 0
+
+        return val
         ### END SOLUTION
 
     def resize(self, newsize):
         assert(len(self.data) < newsize)
         ### BEGIN SOLUTION
+        new = [None] * len(self.data)
+        i = 0
+        for j in self:
+            if j is not None:
+                new[i] = j
+                i += 1
+        new = [None] * (newsize - len(self.data)) + new
+        self.head, self.tail, self.data = -1, len(self.data), new
         ### END SOLUTION
 
     def empty(self):
         ### BEGIN SOLUTION
+        for x in self.data:
+            if x is not None:
+                return False
+        self.head = self.tail = -1
+        return True
         ### END SOLUTION
 
     def __bool__(self):
@@ -194,7 +268,14 @@ class Queue:
 
     def __iter__(self):
         ### BEGIN SOLUTION
+        for x in self.data[self.tail:]:
+            if x is not None:
+                yield x
+        for x in self.data[0:self.tail]:
+            if x is not None:
+                yield x
         ### END SOLUTION
+
 
 ################################################################################
 # QUEUE IMPLEMENTATION - TEST CASES
@@ -218,68 +299,71 @@ def test_queue_implementation_1():
 
     tc.assertTrue(q.empty())
 
+
 # points: 13
 def test_queue_implementation_2():
-	tc = TestCase()
+    tc = TestCase()
 
-	q = Queue(10)
+    q = Queue(10)
 
-	for i in range(6):
-	    q.enqueue(i)
+    for i in range(6):
+        q.enqueue(i)
 
-	tc.assertEqual(q.data.count(None), 4)
+    tc.assertEqual(q.data.count(None), 4)
 
-	for i in range(5):
-	    q.dequeue()
+    for i in range(5):
+        q.dequeue()
 
-	tc.assertFalse(q.empty())
-	tc.assertEqual(q.data.count(None), 9)
-	tc.assertEqual(q.head, q.tail)
-	tc.assertEqual(q.head, 5)
+    tc.assertFalse(q.empty())
+    tc.assertEqual(q.data.count(None), 9)
+    tc.assertEqual(q.head, q.tail)
+    tc.assertEqual(q.head, 5)
 
-	for i in range(9):
-	    q.enqueue(i)
+    for i in range(9):
+        q.enqueue(i)
 
-	with tc.assertRaises(RuntimeError):
-	    q.enqueue(10)
+    with tc.assertRaises(RuntimeError):
+        q.enqueue(10)
 
-	for x, y in zip(q, [5] + list(range(9))):
-	    tc.assertEqual(x, y)
+    for x, y in zip(q, [5] + list(range(9))):
+        tc.assertEqual(x, y)
 
-	tc.assertEqual(q.dequeue(), 5)
-	for i in range(9):
-	    tc.assertEqual(q.dequeue(), i)
+    tc.assertEqual(q.dequeue(), 5)
+    for i in range(9):
+        tc.assertEqual(q.dequeue(), i)
 
-	tc.assertTrue(q.empty())
+    tc.assertTrue(q.empty())
+
 
 # points: 14
 def test_queue_implementation_3():
-	tc = TestCase()
+    tc = TestCase()
 
-	q = Queue(5)
-	for i in range(5):
-	    q.enqueue(i)
-	for i in range(4):
-	    q.dequeue()
-	for i in range(5, 9):
-	    q.enqueue(i)
+    q = Queue(5)
+    for i in range(5):
+        q.enqueue(i)
+    for i in range(4):
+        q.dequeue()
+    for i in range(5, 9):
+        q.enqueue(i)
 
-	with tc.assertRaises(RuntimeError):
-	    q.enqueue(10)
+    with tc.assertRaises(RuntimeError):
+        q.enqueue(10)
 
-	q.resize(10)
+    q.resize(10)
 
-	for x, y in zip(q, range(4, 9)):
-	    tc.assertEqual(x, y)
+    for x, y in zip(q, range(4, 9)):
+        tc.assertEqual(x, y)
 
-	for i in range(9, 14):
-	    q.enqueue(i)
+    for i in range(9, 14):
+        q.enqueue(i)
 
-	for i in range(4, 14):
-	    tc.assertEqual(q.dequeue(), i)
+    for i in range(4, 14):
+        tc.assertEqual(q.dequeue(), i)
 
-	tc.assertTrue(q.empty())
-	tc.assertEqual(q.head, -1)
+    tc.assertTrue(q.empty())
+    tc.assertEqual(q.head, -1)
+
 
 ################################################################################
 # TEST HELPERS
@@ -287,8 +371,10 @@ def test_queue_implementation_3():
 def say_test(f):
     print(80 * "*" + "\n" + f.__name__)
 
+
 def say_success():
     print("SUCCESS")
+
 
 ################################################################################
 # MAIN
