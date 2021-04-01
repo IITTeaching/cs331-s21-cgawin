91a1af1a9becd60e82b3e0354d0a8a0ef9d0bfd0
Cole Gawin <colegawin@gmail.com>
2021-03-31 23:30:56 -0500

    completed lab07

--- lab07/lab07.py
+++ lab07/lab07.py
@@ -1,6 +1,7 @@
 import random
 from unittest import TestCase
 
+
 ################################################################################
 # EXTENSIBLE HASHTABLE
 ################################################################################
@@ -13,19 +14,58 @@ class ExtensibleHashTable:
         self.nitems = 0
 
     def find_bucket(self, key):
-        # BEGIN_SOLUTION
-        # END_SOLUTION
+        ### BEGIN_SOLUTION
+        h = hash(key) % self.n_buckets
 
-    def __getitem__(self,  key):
+        # check if key exists in a bucket
+        while self.buckets[h] is not None:
+            if self.buckets[h][0] == key:
+                return h
+
+            h += 1
+            if h == self.n_buckets:
+                h = 0
+
+        # if not, return original hash
+        return h
+        ### END_SOLUTION
+
+    def __getitem__(self, key):
         # BEGIN_SOLUTION
+        h = self.find_bucket(key)
+
+        if self.buckets[h] is not None:
+            return self.buckets[h][1]
+
+        raise KeyError
         # END_SOLUTION
 
     def __setitem__(self, key, value):
         # BEGIN_SOLUTION
+        h = self.find_bucket(key)
+        self.nitems += 1
+        self.buckets[h] = (key, value)
+
+        if self.nitems > self.n_buckets * self.fillfactor:
+            self.n_buckets *= 2
+            buckets = []
+            i = 0
+
+            while i < len(self.buckets):
+                if self.buckets[i] is not None:
+                    buckets.append(self.buckets[i])
+                i += 1
+            self.buckets = [None] * self.n_buckets
+            for bucket in buckets:
+                h = self.find_bucket(bucket[0])
+                self.buckets[h] = bucket
         # END_SOLUTION
 
     def __delitem__(self, key):
         # BEGIN SOLUTION
+        h = self.find_bucket(key)
+        self.buckets[h] = None
+        self.nitems -= 1
         # END SOLUTION
 
     def __contains__(self, key):
@@ -43,6 +83,9 @@ class ExtensibleHashTable:
 
     def __iter__(self):
         ### BEGIN SOLUTION
+        for bucket in self.buckets:
+            if bucket:
+                yield bucket[0]
         ### END SOLUTION
 
     def keys(self):
@@ -50,10 +93,16 @@ class ExtensibleHashTable:
 
     def values(self):
         ### BEGIN SOLUTION
+        for i in range(self.n_buckets):
+            if self.buckets[i]:
+                yield self.buckets[i][1]
         ### END SOLUTION
 
     def items(self):
         ### BEGIN SOLUTION
+        for i in range(self.n_buckets):
+            if self.buckets[i]:
+                yield self.buckets[i]
         ### END SOLUTION
 
     def __str__(self):
@@ -61,6 +110,7 @@ class ExtensibleHashTable:
 
     def __repr__(self):
         return str(self)
+
 
 ################################################################################
 # TEST CASES
@@ -70,28 +120,29 @@ def test_insert():
     tc = TestCase()
     h = ExtensibleHashTable(n_buckets=100000)
 
-    for i in range(1,10000):
+    for i in range(1, 10000):
         h[i] = i
         tc.assertEqual(h[i], i)
         tc.assertEqual(len(h), i)
 
     random.seed(1234)
     for i in range(1000):
-        k = random.randint(0,1000000)
+        k = random.randint(0, 1000000)
         h[k] = k
         tc.assertEqual(h[k], k)
 
     for i in range(1000):
-        k = random.randint(0,1000000)
+        k = random.randint(0, 1000000)
         h[k] = "testing"
         tc.assertEqual(h[k], "testing")
+
 
 # points: 10
 def test_getitem():
     tc = TestCase()
     h = ExtensibleHashTable()
 
-    for i in range(0,100):
+    for i in range(0, 100):
         h[i] = i * 2
 
     with tc.assertRaises(KeyError):
@@ -102,9 +153,9 @@ def test_getitem():
 def test_iteration():
     tc = TestCase()
     h = ExtensibleHashTable(n_buckets=100)
-    entries = [ (random.randint(0,10000), i) for i in range(100) ]
-    keys = [ k for k, v in entries ]
-    values = [ v for k, v in entries ]
+    entries = [(random.randint(0, 10000), i) for i in range(100)]
+    keys = [k for k, v in entries]
+    values = [v for k, v in entries]
 
     for k, v in entries:
         h[k] = v
@@ -116,12 +167,13 @@ def test_iteration():
     tc.assertEqual(set(values), set(h.values()))
     tc.assertEqual(set(entries), set(h.items()))
 
+
 # points: 20
 def test_modification():
     tc = TestCase()
     h = ExtensibleHashTable()
     random.seed(1234)
-    keys = [ random.randint(0,10000000) for i in range(100) ]
+    keys = [random.randint(0, 10000000) for i in range(100)]
 
     for i in keys:
         h[i] = 0
@@ -133,10 +185,11 @@ def test_modification():
     for k in keys:
         tc.assertEqual(h[k], 10)
 
+
 # points: 20
 def test_extension():
     tc = TestCase()
-    h = ExtensibleHashTable(n_buckets=100,fillfactor=0.5)
+    h = ExtensibleHashTable(n_buckets=100, fillfactor=0.5)
     nitems = 10000
     for i in range(nitems):
         h[i] = i
@@ -153,7 +206,7 @@ def test_deletion():
     tc = TestCase()
     h = ExtensibleHashTable(n_buckets=100000)
     random.seed(1234)
-    keys = [ random.randint(0,1000000) for i in range(10) ]
+    keys = [random.randint(0, 1000000) for i in range(10)]
     for k in keys:
         h[k] = 1
 
@@ -170,14 +223,17 @@ def test_deletion():
     with tc.assertRaises(KeyError):
         h[keys[5]]
 
+
 ################################################################################
 # TEST HELPERS
 ################################################################################
 def say_test(f):
     print(80 * "*" + "\n" + f.__name__)
 
+
 def say_success():
     print("SUCCESS")
+
 
 ################################################################################
 # MAIN
@@ -192,6 +248,7 @@ def main():
         say_test(t)
         t()
         say_success()
+
 
 if __name__ == '__main__':
     main()
