27ab6eab1bb0e9b6a07cf574811fa13225aa1a87
Cole Gawin <colegawin@gmail.com>
2021-05-12 11:01:35 -0500

    completed lab11

--- lab11/lab11.py
+++ lab11/lab11.py
@@ -1,65 +1,120 @@
+import statistics
 from unittest import TestCase
 import random
 
-def quicksort(lst,pivot_fn):
-    qsort(lst,0,len(lst) - 1,pivot_fn)
 
-def qsort(lst,low,high,pivot_fn):
+def quicksort(lst, pivot_fn):
+    qsort(lst, 0, len(lst) - 1, pivot_fn)
+
+
+def partition(lst, low, high, pivot_fn):
+    pivot = pivot_fn(lst, low, high)
+    i = low
+    j = high
+
+    while True:
+        while True:
+            if lst[i] < pivot:
+                i += 1
+            if lst[j] > pivot:
+                j -= 1
+            if not lst[i] < pivot and not lst[j] > pivot:
+                break
+        if i >= j:
+            return j
+        else:
+            lst[i], lst[j] = lst[j], lst[i]
+
+
+def qsort(lst, low, high, pivot_fn):
     ### BEGIN SOLUTION
+    if low < high:
+        partition_index = partition(lst, low, high, pivot_fn)
+
+        qsort(lst, low, partition_index, pivot_fn)
+        qsort(lst, partition_index + 1, high, pivot_fn)
     ### END SOLUTION
 
-def pivot_first(lst,low,high):
+
+def pivot_first(lst, low, high):
     ### BEGIN SOLUTION
+    return lst[low]
     ### END SOLUTION
 
-def pivot_random(lst,low,high):
+
+def pivot_random(lst, low, high):
     ### BEGIN SOLUTION
+    return lst[random.randint(low, high)]
     ### END SOLUTION
 
-def pivot_median_of_three(lst,low,high):
+
+def pivot_median_of_three(lst, low, high):
     ### BEGIN SOLUTION
+    a = lst[low]
+    b = lst[(low + high) // 2]
+    c = lst[high]
+    if a > b:
+        if a < c:
+            return a
+        elif b > c:
+            return b
+        else:
+            return c
+    else:
+        if a > c:
+            return a
+        elif b < c:
+            return b
+        else:
+            return c
     ### END SOLUTION
+
 
 ################################################################################
 # TEST CASES
 ################################################################################
 def randomize_list(size):
-    lst = list(range(0,size))
-    for i in range(0,size):
-        l = random.randrange(0,size)
-        r = random.randrange(0,size)
+    lst = list(range(0, size))
+    for i in range(0, size):
+        l = random.randrange(0, size)
+        r = random.randrange(0, size)
         lst[l], lst[r] = lst[r], lst[l]
     return lst
+
 
 def test_lists_with_pfn(pfn):
     lstsize = 20
     tc = TestCase()
-    exp = list(range(0,lstsize))
+    exp = list(range(0, lstsize))
 
-    lst = list(range(0,lstsize))
+    lst = list(range(0, lstsize))
     quicksort(lst, pivot_first)
-    tc.assertEqual(lst,exp)
+    tc.assertEqual(lst, exp)
 
-    lst = list(reversed(range(0,lstsize)))
+    lst = list(reversed(range(0, lstsize)))
     quicksort(lst, pivot_first)
-    tc.assertEqual(lst,exp)
+    tc.assertEqual(lst, exp)
 
-    for i in range(0,100):
+    for i in range(0, 100):
         lst = randomize_list(lstsize)
         quicksort(lst, pfn)
-        tc.assertEqual(lst,exp)
+        tc.assertEqual(lst, exp)
+
 
 # 30 points
 def test_first():
     test_lists_with_pfn(pivot_first)
 
+
 # 30 points
 def test_random():
     test_lists_with_pfn(pivot_random)
 
+
 # 40 points
 def test_median():
     test_lists_with_pfn(pivot_median_of_three)
+
 
 ################################################################################
 # TEST HELPERS
@@ -67,8 +122,10 @@ def test_median():
 def say_test(f):
     print(80 * "#" + "\n" + f.__name__ + "\n" + 80 * "#" + "\n")
 
+
 def say_success():
     print("----> SUCCESS")
+
 
 ################################################################################
 # MAIN
@@ -81,6 +138,7 @@ def main():
         t()
         say_success()
     print(80 * "#" + "\nALL TEST CASES FINISHED SUCCESSFULLY!\n" + 80 * "#")
+
 
 if __name__ == '__main__':
     main()
